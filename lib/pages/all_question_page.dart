5a84c86a8224d4b33349f0bb9a81f54e821028e8
ningfangqiang <17600407754@163.com>
2022-09-30 11:28:06 +0800

    全部问题页面

--- lib/pages/all_question_page.dart
+++ lib/pages/all_question_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_desktop/model/local_info/currentQuestionInfo.dart';
 
 import '../data/Option_entity.dart';
 import '../data/Question_entity.dart';
+import '../model/questions/failure_probability/part_one/part_one_type1234.dart';
 
 class AllQuestionPage extends StatefulWidget {
   const AllQuestionPage({Key? key}) : super(key: key);
@@ -16,19 +17,19 @@ class _AllQuestionPageState extends State<AllQuestionPage> {
   List catalogueList = [
     {
       "name": "第三方破坏评分",
-      "index": 1,
+      "index": 0,
     },
     {
       "name": "腐蚀评分",
-      "index": 2,
+      "index": 1,
     },
     {
       "name": "设备(装置)及操作的评分",
-      "index": 3,
+      "index": 2,
     },
     {
       "name": "管道本质安全质量的评分",
-      "index": 4,
+      "index": 3,
     },
   ];
 
@@ -40,18 +41,34 @@ class _AllQuestionPageState extends State<AllQuestionPage> {
   ];
 
   /// 当前选择的目录
-  int currentIndex = 1;
-
-  /// 当前的题目列表
-  // List currentQuestionList = [];
+  int currentIndex = 0;
 
   @override
   void initState() {
     super.initState();
   }
 
+  ///
   void switchCatalogue(int i) {
     currentIndex = i;
+    setState(() {});
+  }
+
+  /// 选择某个选项
+  void chooseOption(int questionIndex, OptionEntity option) {
+    /// 如果是第三方破坏评分
+    if (currentIndex == 0) {
+      if (Questioninfo.currentPipelineType == 1 ||
+          Questioninfo.currentPipelineType == 2 ||
+          Questioninfo.currentPipelineType == 3 ||
+          Questioninfo.currentPipelineType == 4) {
+        fPPartOneType1[questionIndex]['isDone'] = 1;
+        fPPartOneType1[questionIndex]['questionScore'] = option.optionScore;
+        fPPartOneType1[questionIndex]['checkedCode'] = option.optionCode;
+      }
+    } else if (currentIndex == 1) {
+      /// 腐蚀评分
+    }
     setState(() {});
   }
 
@@ -103,6 +120,7 @@ class _AllQuestionPageState extends State<AllQuestionPage> {
         height: double.infinity,
         child: Row(
           children: [
+            /// 题目目录
             Container(
               width: 200,
               decoration: const BoxDecoration(
@@ -156,6 +174,8 @@ class _AllQuestionPageState extends State<AllQuestionPage> {
                     .toList(),
               ),
             ),
+
+            /// 题目列表
             Expanded(
               child: Container(
                 width: double.infinity,
@@ -167,13 +187,13 @@ class _AllQuestionPageState extends State<AllQuestionPage> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: currentQuestionList[currentIndex - 1]
+                      children: currentQuestionList[currentIndex]
                           .asMap()
                           .keys
                           .map<Widget>(
-                            (index) => buildSingleQuestion(
-                              currentQuestionList[currentIndex - 1][index],
-                              index,
+                            (questionIndex) => buildSingleQuestion(
+                              currentQuestionList[currentIndex][questionIndex],
+                              questionIndex,
                             ),
                           )
                           .toList(),
@@ -189,19 +209,47 @@ class _AllQuestionPageState extends State<AllQuestionPage> {
   }
 
   /// 单个题目
-  Widget buildSingleQuestion(Map<String, dynamic> singleQuestion, int index) {
+  Widget buildSingleQuestion(
+    Map<String, dynamic> singleQuestion,
+    int questionIndex,
+  ) {
     QuestionEntity enity = QuestionEntity().fromJson(singleQuestion);
     return Container(
       margin: const EdgeInsets.only(bottom: 15),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            '${index + 1}、${enity.questionName}',
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 14,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                '${questionIndex + 1}、${enity.questionName}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  height: 1,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Visibility(
+                visible: enity.isDone == 1,
+                child: const Icon(
+                  Icons.done,
+                  color: Colors.green,
+                  size: 16,
+                ),
+              ),
+              Text(
+                '${enity.questionScore ?? ''}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  height: 1,
+                ),
+              ),
+            ],
           ),
           Padding(
             padding: const EdgeInsets.only(left: 30, top: 5),
@@ -211,7 +259,12 @@ class _AllQuestionPageState extends State<AllQuestionPage> {
                   .asMap()
                   .keys
                   .map(
-                      (index) => buildSingleOption(enity, enity.option![index]))
+                    (optIndex) => buildSingleOption(
+                      enity,
+                      questionIndex,
+                      optIndex,
+                    ),
+                  )
                   .toList(),
             ),
           ),
@@ -221,12 +274,14 @@ class _AllQuestionPageState extends State<AllQuestionPage> {
   }
 
   /// 单个选项
-  Widget buildSingleOption(QuestionEntity question, OptionEntity optionEntity) {
+  Widget buildSingleOption(
+    QuestionEntity question,
+    int questionIndex,
+    int optIndex,
+  ) {
+    OptionEntity optionEntity = question.option![optIndex];
     return GestureDetector(
-      onTap: () {
-        question.checkedCode = optionEntity.optionCode;
-        setState(() {});
-      },
+      onTap: () => chooseOption(questionIndex, optionEntity),
       child: Container(
         alignment: Alignment.centerLeft,
         height: 30,
@@ -237,7 +292,7 @@ class _AllQuestionPageState extends State<AllQuestionPage> {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              '${optionEntity.optionName}(${optionEntity.optionScore}分)',
+              '${optionEntity.optionName}(${optionEntity.optionScore ?? ''}分)',
               style: const TextStyle(
                 fontSize: 12,
                 color: Colors.black,
